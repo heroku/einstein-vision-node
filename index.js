ea206bb8eaaa23c71c9cfc29b03bbefa9411df44
Trevor Scott <tscott@heroku.com>
2016-11-02 13:27:37 -0700

    added image uploader and metamind image identification

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var app = express();
+var express     = require('express');
+var fs          = require('fs');
+var multipart   = require('connect-multiparty');
+var fs          = require('fs');
+var https       = require('https');
+var request     = require('request');
+var querystring = require('querystring');
+
+var app                 = express();
+var multipartMiddleware = multipart();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,6 +20,78 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/file-upload', multipartMiddleware, function(req, res) {
+  //console.log(req.files);
+  // don't forget to delete all req.files when done 
+  fs.readFile(req.files.file.path,'base64', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    // console.log(data);
+    var formData = {
+      modelId: 'GeneralImageClassifier',
+      sampleBase64Content : data
+    }
+    var options = {
+        uri: 'https://api.metamind.io/v1/vision/predict',
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + process.env.METAMIND_TOKEN,
+          'Content-Type': 'multipart/form-data',
+          'Connection': 'keep-alive',
+          'Cache-Control': 'no-cache'
+
+        },
+        formData:formData
+    }
+    
+    request.post(options, function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        return console.error('upload failed:', err);
+      }
+      console.log('status code', httpResponse.statusCode);
+      console.log('headers', httpResponse.headers);
+      console.log('Upload successful!  Server responded with:', body);
+    });
+    // console.log(data);
+    // 'sampleBase64Content' : data,
+    // 'sampleLocation' : 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Matterhorn_Riffelsee_2005-06-11.jpg',
+    /*
+    var post_data = querystring.stringify({
+      'sampleBase64Content' : data,
+      'modelId': 'GeneralImageClassifier'
+    });
+
+    var options = {
+        hostname: 'api.metamind.io',
+        path: '/v1/vision/predict',
+        port: 443,
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + process.env.METAMIND_TOKEN,
+          'Content-Type': 'multipart/form-data',
+          'Content-Length': Buffer.byteLength(post_data),
+          'Cache-Control': 'no-cache'
+        }
+    };
+
+    var req = https.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        console.log("headers: ", res.headers);
+
+        res.on('data', function(d) {
+            console.log('Response: ' + d);
+        });
+    });
+
+    req.write(post_data);
+    console.log('new image uploaded! Sending to metamind...',req);
+    req.end();
+    */
+
+  });
 });
 
 app.listen(app.get('port'), function() {
