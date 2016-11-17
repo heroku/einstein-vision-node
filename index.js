a94981ded42c152ac02043613850bdf3a0c2e64b
Trevor Scott <tscott@heroku.com>
2016-11-17 10:56:52 -0800

    added tests

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const server                = require('./lib/server');
 const fileUpload            = require('./lib/file-upload');
 const updateToken           = require('./lib/update-token');
 const cloudinaryResizeImage = require('./lib/cloudinary-resize-image');
+const readFile              = require('./lib/read-file');
+const oAuthToken            = require('./lib/oauth-token');
+const queryMetamind         = require('./lib/query-metamind');
+const sendImageToMetamind   = require('./lib/send-image-to-metamind');
 
 module.exports = {
   server,
   fileUpload,
   updateToken,
-  cloudinaryResizeImage
+  cloudinaryResizeImage,
+  oAuthToken,
+  queryMetamind,
+  readFile,
+  sendImageToMetamind
 }
