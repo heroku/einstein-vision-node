8457444194dcbcfe8ad0915962e466c005c23bd3
Trevor Scott <tscott@heroku.com>
2016-11-25 21:10:54 -0800

    initial UI/UX updates

--- react-ui/src/app.js
+++ react-ui/src/app.js
@@ -3,9 +3,10 @@ import Dropzone from 'react-dropzone';
 import superagent from 'superagent';
 import classNames from 'classnames';
 import './app.css';
-import InfoLink from './info-link';
+import GithubLink from './github-link';
 import UploadTarget from './upload-target';
 import Predictions from './predictions';
+import Spinner from './spinner';
 
 class App extends Component {
 
@@ -13,45 +14,6 @@ class App extends Component {
     files: [],
     isProcessing: false,
     uploadError: null,
-    /* Example `uploadResponse`:
-      {
-        "probabilities": [
-          {
-            "label": "Heroku",
-            "probability": 0.99
-          },
-          {
-            "label": "infographic",
-            "probability": 0.85
-          },
-          {
-            "label": "street sign",
-            "probability": 0.65
-          },
-          {
-            "label": "digital clock",
-            "probability": 0.20473432540893555
-          },
-          {
-            "label": "odometer, hodometer, mileometer, milometer",
-            "probability": 0.12954171001911163
-          },
-          {
-            "label": "laptop, laptop computer",
-            "probability": 0.07063886523246765
-          },
-          {
-            "label": "cash machine, cash dispenser, automated teller machine, automatic teller machine, automated teller, automatic teller, ATM",
-            "probability": 0.05539492145180702
-          },
-          {
-            "label": "iPod",
-            "probability": 0.04827757552266121
-          }
-        ],
-        "object": "predictresponse"
-      }
-    */
     uploadResponse: null
   }
 
@@ -66,19 +28,11 @@ class App extends Component {
     return (
       <div>
         <div className="title">
-          <h1>Metamind Image Identifier
-            <InfoLink
-              style={{
-                float: 'right',
-                height: '1.1rem',
-                width: '1.1rem'
-              }} 
-              pathStyle={{ 
-                fill: '#999'
-              }}/></h1>
+          <h1 className="intro">
+            Salesforce MetaMind Image Classification
+          </h1>
         </div>
         <div className="app">
-
           <Dropzone
             accept={'image/png, image/jpeg'}
             multiple={false}
@@ -93,28 +47,63 @@ class App extends Component {
             <UploadTarget/>
           </Dropzone>
 
-          <div className={classNames(
-              'status-message',
-              (isProcessing || uploadError) ? 'status-message-visible' : null,
-              uploadError ? 'status-message-error' : null)}>
-            <p>{ uploadError
-              ? uploadError
-              : isProcessing
-                ? 'Processing…' 
-                : null }</p>
-          </div>
-
           
-          <div className={classNames(
+          <div className="result-wrapper">
+            <div className={classNames(
               'image-preview',
-              file != null ? 'image-preview-visible' : null)}
-          ><img
-              alt="upload preview"
-              src={file && file.preview}
-              style={{ display: 'block' }}/></div>
+              file != null ? 'image-preview-visible' : null)}>
+            <Dropzone
+            accept={'image/png, image/jpeg'}
+            multiple={false}
+            onDrop={this.onDrop}
+            style={{}}
+            className={classNames(
+              'dropzone',
+              file != null ? 'dropzone-dropped' : null
+            )}
+            activeClassName="dropzone-active"
+            rejectClassName="dropzone-reject">
+                <img src={file && file.preview} style={{ display: 'block' }}/>
+              </Dropzone>
+                <div className="spinner-wrapper">
+                  {isProcessing
+                        ? <span><Spinner/></span>
+                        : null 
+                  }
+                  {isProcessing
+                    ? <div className="spinner-text">Analyzing Image...</div>
+                    : null
+                  }
+                  {
+                    uploadError
+                    ? uploadError
+                    :null
+                  }
+                </div>
 
-          <Predictions contents={predictions}/>
+                {/*<div className={classNames(
+                    'status-message',
+                    (isProcessing || uploadError) ? 'status-message-visible' : null,
+                    uploadError ? 'status-message-error' : null)}>
+                  <p>
+                    { 
+                      uploadError
+                    ? uploadError
+                    : isProcessing
+                      ? <Spinner/>
+                      : null 
+                    }
+                  </p>
+                </div> */}
+            </div>
+            <Predictions contents={predictions}/>
+          </div>
+        </div>
 
+        <div className="footer">
+          <a href="http://http://metamind.io/">metamind.io</a>
+          <a href="https://github.com/heroku/metamind-image-identifier" 
+             target="_blank">github</a>
         </div>
       </div>
     );
